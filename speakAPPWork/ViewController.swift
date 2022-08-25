f12e0f062419f85ed5f12bbcaa5eeb045f7a7124
Heyi <al6al60212@gmail.com>
2022-08-25 20:38:38 +0800

    Initial Commit

---
+++ speakAPPWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  speakAPPWork
+//
+//  Created by 董禾翊 on 2022/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
